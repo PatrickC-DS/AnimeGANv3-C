59e8449640ed153c8dd4414bd628db013087607f
PatrickC-DS <colin35patrick@gmail.com>
2026-09-20 09:45:26 +0200

    sty_loss * 0.2

--- AnimeGANv3_custom.py
+++ AnimeGANv3_custom.py
@@ -267,7 +267,7 @@ class AnimeGANv3(object) :
         self.con_loss =  con_loss(self.real_photo, self.generated, 0.5)
         # self.s22, self.s33, self.s44  = style_loss_decentralization_3(self.anime_sty_gray, self.fake_sty_gray,  [0.1, 1., 9.])
         self.s22, self.s33, self.s44  = style_loss_decentralization_3(self.anime_sty_gray, self.fake_sty_gray,  [0.1, 5., 25.])
-        self.sty_loss = self.s22  + self.s33 +  self.s44
+        self.sty_loss = 0.2 * (self.s22  + self.s33 +  self.s44)
 
         self.rs_loss =  region_smoothing_loss(self.fake_superpixel, self.generated, 0.2 ) \
                         + VGG_LOSS(self.photo_superpixel, self.generated) * 0.2
